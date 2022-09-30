262e4871e5304b37f1d3e1e93af9485b7969a7f1
Rick Moore <rick329mo@gmail.com>
2022-09-30 16:06:04 -0500

    started adding basic styling

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
   .then((response) => response.json())
   .then((data) => console.log(data));
